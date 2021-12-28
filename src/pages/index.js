bb29011972cacf7480f967ab8893c5edfdb3d7d3
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2021-12-28 07:03:03 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,9 @@
-import * as React from 'react';
+import * as React from "react";
+
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>Hello Frontend Masters!</h1>
+    </main>
+  );
+}
